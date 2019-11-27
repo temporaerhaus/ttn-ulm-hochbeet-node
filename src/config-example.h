6464cf29c91a0ba4bfd52a25573b409d511634b3
Jakob Pietron <jakob.pietron@uni-ulm.de>
2019-11-27 22:02:08 +0100

    move pin mapping config to config.h

--- src/config-example.h
+++ src/config-example.h
@@ -10,3 +10,32 @@ void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 // msb
 static const u1_t PROGMEM APPKEY[16] = { };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+// Pin Mappings
+
+// Adafruit Feather M0
+const lmic_pinmap lmic_pins = {
+    .nss = 8,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = LMIC_UNUSED_PIN,
+    .dio = {3, 6, LMIC_UNUSED_PIN},
+};
+
+
+// Pinmapping for Dragino Arduino shield
+/*const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, 7},
+};
+*/
+// Pinmapping for LSN50 ??
+/* 
+const lmic_pinmap lmic_pins = {
+        .nss =  PA15,         //38, PA15 pin
+        .rxtx = LMIC_UNUSED_PIN,    
+        .rst = PB0,        // 18,PB0
+        .dio = {2,21,22},  //PC13,PB10,PB11
+};
+*/
--- src/hochbeet.cpp
+++ src/hochbeet.cpp
@@ -66,32 +66,6 @@ bool joined = false;
 static osjob_t sendjob;
 const unsigned TX_INTERVAL = 150;
 
-// Adafruit Feather M0
-const lmic_pinmap lmic_pins = {
-    .nss = 8,
-    .rxtx = LMIC_UNUSED_PIN,
-    .rst = LMIC_UNUSED_PIN,
-    .dio = {3, 6, LMIC_UNUSED_PIN},
-};
-
-// Pinmapping for Dragino Arduino shield
-/*const lmic_pinmap lmic_pins = {
-    .nss = 10,
-    .rxtx = LMIC_UNUSED_PIN,
-    .rst = 9,
-    .dio = {2, 6, 7},
-};
-*/
-// Pinmapping for LSN50 ??
-/* 
-const lmic_pinmap lmic_pins = {
-        .nss =  PA15,         //38, PA15 pin
-        .rxtx = LMIC_UNUSED_PIN,    
-        .rst = PB0,        // 18,PB0
-        .dio = {2,21,22},  //PC13,PB10,PB11
-};
-*/
-
 //***************************
 // Pins und Sensoren
 //***************************
